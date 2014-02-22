d30ac2177b49898528eae03ee6c65de6e664c7f1
martinez <martinez666@msn.com>
2014-02-22 23:58:45 +1100

    split screen improvements

--- javascripts/splitScreen.js
+++ javascripts/splitScreen.js
@@ -2,18 +2,41 @@
 SplitScreenRenderer = function() {
     this.render = function(renderer, scenes, cameras, screenWidth, screenHeight) {
         
-        var axisCount = Math.ceil(Math.sqrt(scenes.length));
-        var width = screenWidth / axisCount;
-        var height = screenHeight / axisCount;
+        var xAxisCount = Math.ceil(Math.sqrt(scenes.length));
+        var width = screenWidth / xAxisCount;
         
-        var left = 0;
+        var yAxisCount;
+        var height;
+        
+        if(scenes.length <= (xAxisCount * (xAxisCount - 1))) {
+            yAxisCount = xAxisCount - 1;
+        } else {
+            yAxisCount = xAxisCount;
+        }
+        
+        height = screenHeight / yAxisCount;
+        
+        
+        var screenAspectRatio = screenWidth / screenHeight;
+        var aspectRatio = width / height;
+        
+        if (aspectRatio > screenAspectRatio) {
+            width = height * screenAspectRatio;
+        } else {
+            height = width / screenAspectRatio;
+        }
+        
+        
         var sceneIndex = 0;
         
-        for (var i = 0; i < axisCount; ++i, left += width) {
             
-            var bottom = 0;
+        var bottom = (screenHeight - height * yAxisCount) / 2;
+        
+        for(var j = 0; j < yAxisCount && sceneIndex < scenes.length; ++j, bottom += height) {
             
-            for(var j = 0; j < axisCount; ++j, ++sceneIndex, bottom += height) {
+            var left = (screenWidth - width * xAxisCount) / 2;
+        
+            for (var i = 0; i < xAxisCount && sceneIndex < scenes.length; ++i, ++sceneIndex, left += width) {
                 renderer.setViewport( left, bottom, width, height );
                 renderer.render(scenes[sceneIndex], cameras[sceneIndex], null, false);
             }
