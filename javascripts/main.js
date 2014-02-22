92d4f3eda2c2d00ffd567ad4a2bb3c5fd5abaef4
Cong <congusbongus@gmail.com>
2014-02-22 16:54:00 +1100

    Player now turns in directions, add texture

--- javascripts/main.js
+++ javascripts/main.js
@@ -46,19 +46,7 @@ function render() {
   requestAnimationFrame(render);
   
   // handle input
-  if ( keysPressed.left ) {
-    player.dir.x = -1;
-    player.dir.y = 0;
-  } else if ( keysPressed.right ) {
-    player.dir.x = 1;
-    player.dir.y = 0;
-  } else if ( keysPressed.up ) {
-    player.dir.x = 0;
-    player.dir.y = 1;
-  } else if ( keysPressed.down ) {
-    player.dir.x = 0;
-    player.dir.y = -1;
-  }
+  player.setDir( keysPressed );
   keysPressed = {};
   player.update();
   
